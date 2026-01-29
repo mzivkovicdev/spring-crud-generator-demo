7621e5f7a0daef36345eaae774b3b306650a9f59
mzivkovicdev <mzivkovic.dev@gmail.com>
2026-01-29 19:57:09 +0100

    init the project and add crud spec yaml

---
+++ src/main/java/dev/markozivkovic/spring_crud_generator_demo/SpringCrudGeneratorDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.markozivkovic.spring_crud_generator_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudGeneratorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudGeneratorDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/markozivkovic/spring_crud_generator_demo/SpringCrudGeneratorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.markozivkovic.spring_crud_generator_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudGeneratorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
